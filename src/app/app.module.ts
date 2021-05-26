85f9da3e6b3b7765f790e96272fc673785bd64f9
Marcela <marcelapereirads@gmail.com>
2021-05-25 22:21:03 -0300

    Skills component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { AboutComponent } from './components/about/about.component';
-import { AvatarComponent } from './components/about/avatar/avatar.component';
-import { InfoComponent } from './components/about/info/info.component';
 import { AboutModule } from './components/about/about.module';
+import { SkillsModule } from './components/skills/skills.module';
 
 @NgModule({
   declarations: [
@@ -15,7 +13,8 @@ import { AboutModule } from './components/about/about.module';
   ],
   imports: [
     BrowserModule,
-    AboutModule
+    AboutModule,
+    SkillsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/skills/skills.component.ts
@@ -0,0 +1,38 @@
+import {Component} from '@angular/core';
+
+export interface Tile {
+  color: string;
+  bgColor: string;
+  cols: number;
+  rows: number;
+  text: string;
+}
+
+@Component({
+  selector: 'app-skills',
+  templateUrl: './skills.component.html',
+  styleUrls: ['./skills.component.scss']
+})
+export class SkillsComponent {
+  
+  readonly colors = {
+    yellow: '#F2EC89',
+    lightGray: '#999797',
+    darkGray: '#58555A',
+    black: '#000000',
+    white: '#ffffff'
+  };
+
+  tiles: Tile[] = [
+    { text: 'CSS', cols: 1, rows: 3, bgColor: this.colors.lightGray, color: this.colors.darkGray },
+    { text: 'Bootstrap', cols: 1, rows: 3, bgColor: this.colors.darkGray, color: this.colors.black },
+    { text: 'jQuery', cols: 1, rows: 3, bgColor: this.colors.lightGray, color: this.colors.darkGray },
+    { text: 'Angular', cols: 3, rows: 3, bgColor: this.colors.yellow, color: this.colors.darkGray },
+    { text: 'React', cols: 1, rows: 3, bgColor: this.colors.darkGray, color: this.colors.black },
+    { text: 'Javascript', cols: 2, rows: 3, bgColor: this.colors.black, color: this.colors.white },
+    { text: 'Jest', cols: 1, rows: 4, bgColor: this.colors.black, color: this.colors.white },
+    { text: 'SASS', cols: 1, rows: 4, bgColor: this.colors.darkGray, color: this.colors.black },
+    { text: 'HTML', cols: 1, rows: 5, bgColor: this.colors.lightGray, color: this.colors.darkGray },
+    { text: 'Testing Library', cols: 2, rows: 1, bgColor: this.colors.yellow, color: this.colors.darkGray },
+  ];
+}
