f4acc27c51ffcddb7097e4714b437bc315f73019
Marcela Silva <Marcela.Silva@ctw.bmwgroup.com>
2024-03-02 01:05:58 +0000

    Install ngx-timeline

--- src/app/components/timeline/timeline.component.ts
+++ src/app/components/timeline/timeline.component.ts
@@ -1,21 +1,50 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import {
+  NgxTimelineEvent,
+  NgxTimelineEventChangeSideInGroup,
+  NgxTimelineModule,
+} from '@frxjs/ngx-timeline';
 
 @Component({
   selector: 'app-timeline',
   templateUrl: './timeline.component.html',
   styleUrls: ['./timeline.component.scss'],
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgxTimelineModule],
+  encapsulation: ViewEncapsulation.None,
 })
 export class TimelineComponent {
-  readonly experiences = [
-    'Critical Teckworks',
-    'Aubay Portugal S.A.',
-    'Zup Innovation',
-    'TQI Consultoria e Desenvolvimento',
-    'Jaraguá Tecnologia',
-    'Neppo Soluções em TI',
-    'Plena Serviços Contábeis',
+  readonly changeSide = NgxTimelineEventChangeSideInGroup.ALL;
+
+  readonly events: Array<NgxTimelineEvent> = [
+    {
+      timestamp: new Date('2023-08'),
+      title: 'Critical Teckworks',
+    },
+    {
+      timestamp: new Date('2021-10'),
+      title: 'Aubay Portugal S.A.',
+    },
+    {
+      timestamp: new Date('2021-04'),
+      title: 'Zup Innovation',
+    },
+    {
+      timestamp: new Date('2020-02'),
+      title: 'TQI Consultoria e Desenvolvimento',
+    },
+    {
+      timestamp: new Date('2019-08'),
+      title: 'Jaraguá Tecnologia',
+    },
+    {
+      timestamp: new Date('2016-01'),
+      title: 'Neppo Soluções em TI',
+    },
+    {
+      timestamp: new Date('2014-10'),
+      title: 'Plena Serviços Contábeis',
+    },
   ];
 }
